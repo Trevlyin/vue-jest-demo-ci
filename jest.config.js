9d919786969db771fe8b593573072240855040f3
Trevlyin <trevlyin.muzinda@fbc.co.zw>
2021-10-31 19:09:44 +0200

    work in progress

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports ={
+
+    moduleFileExtensions: ['js', 'vue'],
+    transform: {
+        '^.+\\.vue$': 'vue-jest',
+        '^.+\\.(js|jsx)?$': 'babel-jest'
+    },
+    transformIgnorePatterns: ["<rootDir>/node_modules/"]
+}
